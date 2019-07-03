8ad67c77a4fc76663ee28e82b50217beda2d0a41
anshkamra <ansh.kamra@lifcare.in>
2019-07-04 00:10:57 +0530

    update

--- installation.sh
+++ installation.sh
@@ -5,7 +5,9 @@ chmod u+x install-helm.sh
 ./install-helm.sh
 kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+sleep 3
 helm init --service-account tiller
+sleep 5
 
 # Installing MongoDB using Helm chart with Persistent Volume:
 helm install --name mongo --set mongodbRootPassword=AfODN3Rt55,mongodbUsername=root,mongodbPassword=AfODN3Rt55,mongodbDatabase=sample stable/mongodb
